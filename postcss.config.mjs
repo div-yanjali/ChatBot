36390bcb3f6e36a89b4866b9395f9f762c7f4d87
div-yanjali <143691107+div-yanjali@users.noreply.github.com>
2025-06-09 22:57:26 +0530

    Add files via upload

---
+++ postcss.config.mjs
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+=======
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
+>>>>>>> 4ea17be (Initial commit)
+};
+
+export default config;
